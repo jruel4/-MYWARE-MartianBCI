d6682e13ebf2a0e6115085e6577000df2fea28cb
martianmartin <flowt-biohacker@gmail.com>
2017-07-17 21:15:09 -0400

    Updated noise reject block for variable input size

--- MartianBCI/Blocks/block_noise_reject.py
+++ MartianBCI/Blocks/block_noise_reject.py
@@ -6,41 +6,48 @@ Created on Mon Jul 17 05:32:34 2017
 @author: marzipan
 """
 
-# -*- coding: utf-8 -*-
-"""
-Possible approached to high amplitude noise rejection.
-
-1) discretely epoched, such that there is a definite extra delay...
-
-2) continuous ... perhaps just replacing with average?
-
-We should focus on a specific pipeline, and clean for that 
-"""
-
-from scipy import signal
-from .Block import Block
+if __name__ == "__main__":
+    from Block import Block
+else:
+    from .Block import Block
 import numpy as np
+from collections import deque
 
 class block_noise_reject (Block):
     
     def __init__(self, _pipe):   
         self.mPipe = _pipe
         
+        self.EPOCH_LEN = 250
+        self.NUM_CHAN = 8
+        self.NUM_SAMPLES_IN = 1
         self.NOISE_THRESH = 150e-6 # Volts
         self.B2V = 4.5/(2**23-1)/24.0
         
+        self._buf = deque([np.zeros(self.NUM_CHAN) for i in range(self.EPOCH_LEN)], maxlen=self.EPOCH_LEN)
+        
     def run(self, inbuf):
       
-      # Input buf should be a deque of 250 samples
-      d = np.asarray(inbuf)
-      assert d.shape == (250,8)
+      #TODO Process inbuf with super method
+      #TODO add smart check to ensure input is not already converted to Volts
       
-      # convert to volts
+      # Check input dimensions
+      d = np.asarray(inbuf)
+      assert d.shape == (self.NUM_CHAN, self.NUM_SAMPLES_IN)
+      
+      # Split samples to add to buff
+      list_of_samples = d.transpose().tolist()
+      self._buf.extend(list_of_samples)
+      
+      # Convert deque to array
+      d = np.asarray(self._buf).transpose() # num_chan x num_samples
+      
+      # convert to voltnum
       d *= self.B2V
       
       # get min, max of each column to see if artifact
-      mins = d.min(axis=0)
-      maxs = d.max(axis=0)
+      mins = d.min(axis=1)
+      maxs = d.max(axis=1)
       
       valid = (maxs - mins) < self.NOISE_THRESH
       
@@ -48,13 +55,10 @@ class block_noise_reject (Block):
               'data': d}
       
     def get_output_struct(self):
-        return {'filtered':8}
+        return {'valid':(self.NUM_CHAN),
+                'data':(self.NUM_CHAN, self.EPOCH_LEN)}
       
-    def set_filter(self, f1, f2):
-      self.fir_coeffs = signal.firwin(self.n_taps, [f1, f2], pass_zero=False, nyq=125.0)
-    
-    
-    
+
     
     
     
