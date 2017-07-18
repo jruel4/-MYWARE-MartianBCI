274b91e8f0fb4a27444419fcc642d1436c05e37f
martianmartin <flowt-biohacker@gmail.com>
2017-07-17 20:06:04 -0400

    morlet block complete (tested)

--- MartianBCI/Blocks/block_morlet.py
+++ MartianBCI/Blocks/block_morlet.py
@@ -6,25 +6,38 @@ Created on Mon Jul 17 07:25:48 2017
 @author: marzipan
 """
 
-from .Block import Block
+if __name__ == "__main__":
+    from Block import Block
+else:
+    from .Block import Block
 import numpy as np
 from MartianBCI.Utils.wavelets import make_morlet
 
 class block_morlet (Block):
     
-    def __init__(self, _pipe):   
+    def __init__(self, _pipe, f_fwhm=[(12,4)]):  
+        '''
+        f_fwhm is a list of tuples where each tuple is (frequency, full-width-half-max)
+        '''
         self.mPipe = _pipe
         # create wavelet 
-        f = 12.0 # frequency (Hz)
-        fwhm = 4 # must be 2,4 or 6
-        self.bm = make_morlet(f,fwhm)
+        self.wavelets_mat = np.asarray([make_morlet(f,fwhm) for f,fwhm in f_fwhm]).transpose()
+        self.num_freqs = len(f_fwhm)
+        assert self.wavelets_mat.shape == (250, self.num_freqs)
         
     def run(self, inbuf):
       # Input buf should be a np array of shape
-      assert inbuf.shape == (250, 8)
-      power = np.abs(np.dot(self.bm,inbuf)) # Volts
+      assert inbuf.shape == (8, 250)
+      power = np.abs(np.dot(inbuf, self.wavelets_mat)) # Volts
       return {'default':power}
       
     def get_output_struct(self):
-        return {'filtered':8}
+        return {'default' : (8,self.num_freqs)}
+      
+      
+      
+      
+      
+      
+      
       
